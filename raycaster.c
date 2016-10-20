360237155b927e447ccbf7366028680e4c54cb07
Beckcjb <cjb456@nau.edu>
2016-10-19 21:09:36 -0700

    Commenting
    Added description and comments to raycaster and parser
    Updated Objects File

--- raycaster.c
+++ raycaster.c
@@ -1,7 +1,7 @@
 // Charles Beck
 // CS 430
-// 10/4/16
-// PROJECT 2: RAYCASTER
+// 10/16/16
+// PROJECT 3: RAYCASTER With ILLUMINATION
 // ==================================================================================
 /* 	   
 	   This program reads in five arguments from the comand line in the order of :
@@ -12,7 +12,9 @@
    Then the program will parse the deisred JSON file while error checking for a 
    well made and valid JSON file. After the program gets through parsing, the data
    is stored into structures that will allow the program to test intersections of pixels 
-   with a ray that is constructed and shot at every pixel in the scene. The program will
+   with a ray that is constructed and shot at every pixel in the scene. The program will add
+   lighting and shadows to the objects based off of intersections with each other and the
+   light ray vector. This is done with various illimination porperties and formulas. The program will
    store the intersection data and then write it to a P3 ppm file format with color. 
 */  
 // ==================================================================================
